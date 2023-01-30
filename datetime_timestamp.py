013839827f749efb0f12e50e848a3bc0f81ebc02
sapnasb <122716785+sapnasb@users.noreply.github.com>
2023-01-30 23:10:54 +0530

    Add files via upload
    This program prints both datetime and timestamp.

---
+++ datetime_timestamp.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+now = datetime.now()
+ts = datetime.timestamp(now)
+
+print('Current DateTime:', now)
+print("Timestamp is:", ts)
+
+'''
+Output:
+Current DateTime: 2023-01-30 23:08:48.642103
+Timestamp is: 1675100328.642103
+
+'''
+ 
